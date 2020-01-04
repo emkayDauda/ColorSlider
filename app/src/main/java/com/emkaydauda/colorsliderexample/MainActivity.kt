87951c4bfe997e0dae38e1f14e5f8014ccc03d00
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-04 19:58:07 +0100

    :bento: Complete Sample App

--- app/src/main/java/com/emkaydauda/colorsliderexample/MainActivity.kt
+++ app/src/main/java/com/emkaydauda/colorsliderexample/MainActivity.kt
@@ -2,11 +2,16 @@ package com.emkaydauda.colorsliderexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textBackground.addListener {
+            display.setBackgroundColor(it)
+        }
     }
 }
