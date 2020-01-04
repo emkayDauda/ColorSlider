641c796b9a2a508b547d9ad7881dda0d6813a216
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-04 20:05:02 +0100

    :bug: Use better variable names

--- app/src/main/java/com/emkaydauda/colorsliderexample/MainActivity.kt
+++ app/src/main/java/com/emkaydauda/colorsliderexample/MainActivity.kt
@@ -10,8 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textBackground.addListener {
-            display.setBackgroundColor(it)
+        colorSlider.addListener {
+            textDisplay.setBackgroundColor(it)
         }
     }
 }
